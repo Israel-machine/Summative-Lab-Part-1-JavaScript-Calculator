f45f9c1190eecceff1316ff318f45d1b1476ee56
Israel-machine <israel.diaz1701@gmail.com>
2026-02-07 19:09:37 -0800

    jsCalculator feedback updates

--- jsCalculator.js
+++ jsCalculator.js
@@ -1,20 +1,20 @@
 function add(num1, num2) {
-    let sumResult = num1 + num2;
+    const sumResult = num1 + num2;
     return sumResult;
 };
 
 function subract(num1, num2) {
-    let subResult = num1 - num2;
+    const subResult = num1 - num2;
     return subResult;
 };
 
 function multiply(num1, num2) {
-    let multResult = num1 * num2;
+    const multResult = num1 * num2;
     return multResult;
 };
 
 function divide(num1, num2) {
-    let divResult = num1 / num2;
+    const divResult = num1 / num2;
     return divResult;
 };
 
@@ -22,7 +22,7 @@ const historyLog = [];
 
 function addToHistory(oppFunc, num1, num2) {
     const operationResult = oppFunc(num1, num2)
-    historyLog.push(operationResult)
+    historyLog.push({operands: [num1, num2], operator: oppFunc.name, result: operationResult})
 };
 
 function displayLog() {
@@ -32,3 +32,9 @@ function displayLog() {
         console.log(historyLog)
     }
 };
+
+addToHistory(add, 2,3);
+addToHistory(subract, 2,3);
+addToHistory(multiply, 2,3);
+addToHistory(divide, 2,3);
+displayLog();
